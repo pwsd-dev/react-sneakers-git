8f1cddc6c1051e3b9630414b4b691a9ee4ff30c3
Nikita Shvetsov <nshvtsv@gmail.com>
2021-12-06 04:26:14 -0800

    add

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
 function App() {
   return (
     <div className="wrapper">
-      <header>
-        <div className="wrapper">
-          <div className="left">
-            <img width={40} height={40} src="/img/logo.png" />
-            <div className="column">
-              <h3>React Sneakers</h3>
-              <p>Магазин лучших кроссовок</p>
-            </div>
+      <header className="d-flex justify-between p-40">
+        <div className="left">
+          <img width={40} height={40} src="/img/logo.png" className="mr-15" />
+          <div className="column">
+            <h3>React Sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
           </div>
-          <div className="right">
-            <ul>
-              <li>
-                <img width={20} height={20} src="/img/icon-telegka.png" />
-
-              </li>
-              <li>
-                <p><span>1205руб</span></p>
-              </li>
-              <li>
-                <img width={20} height={20} src="/img/icon-heart.png" />
-
-              </li>
-              <li>
-                <img width={20} height={20} src="/img/icon-profile.png" />
-
-              </li>
-
-            </ul>
-          </div>
+        </div>
+        <div className="right">
+          <ul>
+            <li>
+              <img width={20} height={20} src="/img/icon-telegka.png" />
+            </li>
+            <li>
+              <p><span>1205руб</span></p>
+            </li>
+            <li>
+              <img width={20} height={20} src="/img/icon-heart.png" />
+            </li>
+            <li>
+              <img width={20} height={20} src="/img/icon-profile.png" />
+            </li>
+          </ul>
         </div>
       </header>
       <div className="content">
