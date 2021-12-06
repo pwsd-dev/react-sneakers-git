a2adcbe6f6a14ea45b15c9c71fcaa7e06ecce0e3
Nikita Shvetsov <nshvtsv@gmail.com>
2021-12-05 19:04:19 -0800

    add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-// import logo from './logo.svg';
-
-
 function App() {
   return (
-    <div className="App">Начал проект</div>
+    <div className="wrapper">
+      <header>
+        <div className="wrapper">
+          <div className="left">
+            <img src="/img/logo.png" />
+            <div className="column">
+              <h3>React Sneakers</h3>
+              <p>Магазин лучших кроссовок</p>
+            </div>
+          </div>
+          <div className="right">
+            <ul>
+              <li>
+                <img src="/img/icon-telegka.png" />
+
+              </li>
+              <li>
+                <p><span>1205руб</span></p>
+              </li>
+              <li>
+                <img src="/img/icon-heart.png" />
+
+              </li>
+              <li>
+                <img src="/img/icon-profile.png" />
+
+              </li>
+
+            </ul>
+          </div>
+        </div>
+      </header>
+      <div className="content">
+        <h1>Все кроссовки</h1>
+      </div>
+    </div >
   );
 }
 
