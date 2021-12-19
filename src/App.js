021c210cb9f0ca54b553ee61bd5cb4c431856302
Nikita Shvetsov <nshvtsv@gmail.com>
2021-12-19 06:36:42 -0800

    add

--- src/App.js
+++ src/App.js
@@ -3,34 +3,34 @@ import Card from './components/Card';
 import Cart from './components/Cart'
 import Header from './components/Header';
 
-const arrSneakers = [
-  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: '/img/sneakers/1.png', id: 1 },
-  { title: 'Мужские Кроссовки Nike Air Max 270', price: 5799, imageUrl: '/img/sneakers/2.png', id: 2 },
-  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8599, imageUrl: '/img/sneakers/3.png', id: 3 },
-  { title: 'Мужские Кроссовки Under Armour Curry 8', price: 9799, imageUrl: '/img/sneakers/4.png', id: 4 },
-  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 15199, imageUrl: '/img/sneakers/5.png', id: 5 },
-  { title: 'Мужские Кроссовки Nike Kyrie 7', price: 5799, imageUrl: '/img/sneakers/6.png', id: 6 },
-  { title: 'Мужские Кроссовки Jordan Air Jordan 11', price: 12599, imageUrl: '/img/sneakers/7.png', id: 7 },
-  { title: 'Мужские Кроссовки Nike LeBron XVIII', price: 9799, imageUrl: '/img/sneakers/8.png', id: 8 },
-]
-
 function App() {
+  let [items, setItems] = React.useState([]);
+  let [cartOpened, setCartOpened] = React.useState(false);
+  let [cartItem, setCartItem] = React.useState([]);
 
-  const [cartOpened, setCartOpened] = React.useState(false);
-  const [items, setItems] = React.useState();
-  const [cartItem, setCartItem] = React.useState([
-    { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: '/img/sneakers/1.png', id: 1 },
-    { title: 'Мужские Кроссовки Nike Air Max 270', price: 5799, imageUrl: '/img/sneakers/2.png', id: 2 },
-  ]);
-
-  const onAddToCart = (obj) => {
-    setCartItem([...cartItem], obj)
+  let onAddToCart = (obj) => {
+    setCartItem([...cartItem, obj]);
+    // console.log(obj);
   }
+
+  React.useEffect(() => {
+    fetch('https://61bf2889b25c3a00173f4cbe.mockapi.io/items')
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setItems(json);
+      });
+  }, []);
 
   console.log(cartItem);
 
-  let card = arrSneakers.map((item, index) => {
-    return (<Card title={item.title} imageUrl={item.imageUrl} price={item.price} key={index} onPlus={(obj) => onAddToCart(obj)} />);
+  let card = items.map((item, index) => {
+    return (<Card title={item.title}
+      imageUrl={item.imageUrl}
+      price={item.price}
+      key={index}
+      onPlus={(item) => onAddToCart(item)} />);
   });
 
   return (
--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -2,20 +2,22 @@ import React from 'react';
 
 function Cart({ onCloseCart, items = [] }) { // из объекта props с помощью деструктуризации берем переменную onCloseCart и items который по умолчанию пустой массив
     // const [cartOpened, setCardOpened] = React.useState(false);
+    // const [items, setItems] = React.useState();
     return (
         <div className="cart">
             <div className="background"></div>
             <div className="side p-30">
                 <b className="" onClick={onCloseCart}>Корзина</b>
                 <div className="items">
-                    {items.map((obj) => {
-                        <div className="item">
-                            <div className="left-side">
-                                <img width={70} height={70} src={obj.imageUrl} alt="sneakers"></img>
-                            </div>
-                            <div className="mid-side">2</div>
-                            <div className="right-side"></div>
-                        </div>
+                    {items.map((obj, index) => {
+                        return (
+                            <div className="item">
+                                <div className="left-side">
+                                    <img width={70} height={70} src={obj.imageUrl} alt="sneakers"></img>
+                                </div>
+                                <div className="mid-side">{obj.title}</div>
+                                <div className="right-side">{obj.price}</div>
+                            </div>)
                     })}
                 </div>
             </div>
